deb2a8e20a27cac2a879d23ca2c1fb8ce493951c
revanthbudida04-hash <revanthbudida04@gmail.com>
2026-02-24 18:37:50 +0530

    Create grade-calculator.py

---
+++ grade-calculator.py
@@ -0,0 +1,10 @@
+m=int(input("enter your marks in math: "))
+e=int(input("enter your marks in english: "  ))
+c=int(input("enter your marks in chemistry: "))
+p=int(input("enter your marks in physics: "))
+pro=int(input("enter your marks in programming: "))
+t=m+e+c+p+pro
+print("total=",t)
+n=5
+ave=t/n
+print("your average marks is",ave)
